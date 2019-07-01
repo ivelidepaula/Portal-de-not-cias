7a35e756a21b64af681bf5f38e07787901888fdc
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-07-01 05:05:01 +0200

    install ejs

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("<html><body>Portal de notícias</body></html>")
-});
+app.set('view engine', 'ejs');
 
 app.get('/tecnologia', function(req, res){
-    res.send("<html><body>Notícias de tecnologia</body></html>")
+    res.render("secao/tecnologia");
+});
+
+app.get('/', function(req, res){
+    res.send("<html><body>Portal de notícias</body></html>");
 });
 
 app.listen(3000, function(){
