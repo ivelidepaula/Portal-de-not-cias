ea9223438aa8af262d237dd0715732d140f66154
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-07-01 03:48:47 +0200

    Require and create server

--- noticias.js
+++ noticias.js
@@ -1,1 +1,7 @@
-console.log('Criando um site de notícias com Node.js');
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.end("<html><body>Portal de notícias</body></html>");
+});
+
+server.listen(3000);
