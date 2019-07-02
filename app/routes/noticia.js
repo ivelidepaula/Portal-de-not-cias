ddff123b269fcb83329be189655876bedb6b915a
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-07-03 00:28:03 +0200

    implement models

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,11 +1,13 @@
 module.exports = function(app) {
+	
+	app.get('/noticia', function(req, res){
+	
+		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
 
-    app.get('/noticia', function(req, res){
+		noticiasModel.getNoticia(connection, function(error, result){
+			res.render("noticias/noticia", {noticia : result});
+		});	
+	});
 
-        var connection = app.config.dbConnection();
-        
-        connection.query('select * from noticias where id_noticia = 2', function(error, result){
-            res.render("noticias/noticia", {noticias : result});
-        });
-    });
 };
