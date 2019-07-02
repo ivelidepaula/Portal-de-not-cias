a93c09a77cb03c2716f597bde2bd5a1c55c837fa
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-07-02 23:45:59 +0200

    refactor database

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function(app) {
+var connMySQL = function(){
+    console.log('A conexao com o BD foi estabelecida')
     return mysql.createConnection({
         host : 'localhost',
         user : 'root',
         password : '1234',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function() {
+    console.log('O autoload carregou o módulo')
+    return connMySQL;
 }
