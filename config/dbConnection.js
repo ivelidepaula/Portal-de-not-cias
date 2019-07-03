d3f01bb276a43650e6267bb25ea8b5949a1f380a
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-07-04 00:05:23 +0200

    implement classes

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,16 +1,14 @@
 var mysql = require('mysql');
 
 var connMySQL = function(){
-    console.log('A conexao com o BD foi estabelecida')
-    return mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : '1234',
+    return connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
         database: 'portal_noticias'
     });
 }
 
-module.exports = function() {
-    console.log('O autoload carregou o módulo')
+module.exports = function(){
     return connMySQL;
 }
