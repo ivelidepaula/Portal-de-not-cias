a7e9708999589a04e94ec5fef1bc42e3109ae0f8
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-07-04 18:23:38 +0200

    fix typos

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,6 @@
 module.exports = function(application){
-    
-    application.get('/', function(req,res){
-        application.app.controllers.home.index(application, req, res);
-    });
+
+	application.get('/', function(req, res){
+		application.app.controllers.home.index(application, req, res);
+	});
 }
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,11 +1,11 @@
-module.exports = function(application){
+module.exports = function(application) {
+	
+	application.get('/noticias', function(req, res){
+		application.app.controllers.noticias.noticias(application, req, res);		
+	});
 
-    application.get('/noticias', function(req,res){
-        application.app.controllers.noticias.noticias();
+	application.get('/noticia', function(req, res){
+		application.app.controllers.noticias.noticia(application, req, res);
+	});
 
-    });
-
-    application.get('/noticia', function(req,res){
-        application.app.controllers.noticias.noticia();
-    });
-}
+};
